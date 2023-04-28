bc277ff01cfd4a2a9273f0b2085697cc15207f19
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-28 22:03:44 +0300

    author entity created

---
+++ bookstore/src/main/java/com/example/bookstore/entity/Author.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
+}
