a6a8118cec4e2a8aa3ab8022840a5d6ccbbc0238
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-29 21:49:21 +0300

    update

--- bookstore/src/main/java/com/example/bookstore/repository/AuthorRepository.java
+++ bookstore/src/main/java/com/example/bookstore/repository/AuthorRepository.java
@@ -4,4 +4,5 @@ import com.example.bookstore.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    boolean existsByNameIgnoreCase(String name);
 }
