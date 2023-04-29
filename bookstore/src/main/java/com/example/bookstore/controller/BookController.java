ce8588ca02cca9ec1ab66f8c320c582f263ffd1e
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-29 21:23:20 +0300

    BookController.java created

---
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,35 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.business.BookService;
+import com.example.bookstore.entity.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping
+    public Book create(@RequestBody Book book) {
+        return bookService.add(book);
+    }
+
+    @GetMapping
+    public List<Book> getAll() {
+        return bookService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getById(@RequestParam int id) {
+        return bookService.getById(id);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestParam int id) {
+        bookService.delete(id);
+    }
+}
