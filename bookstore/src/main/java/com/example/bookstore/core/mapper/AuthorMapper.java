22612959216b17ae86ddee94a1aa6a34ebfb0092
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-01 17:39:37 +0300

    mapper update

--- bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
+++ bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
@@ -2,8 +2,11 @@ package com.example.bookstore.core.mapper;
 
 import com.example.bookstore.core.dto.responses.AuthorDTO;
 import com.example.bookstore.entity.Author;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface AuthorMapper {
     AuthorDTO authorToAuthorDTO(Author author);
+
     Author authorDTOToAuthor(AuthorDTO authorDTO);
 }
--- bookstore/src/main/java/com/example/bookstore/core/mapper/BookMapper.java
+++ bookstore/src/main/java/com/example/bookstore/core/mapper/BookMapper.java
@@ -2,8 +2,15 @@ package com.example.bookstore.core.mapper;
 
 import com.example.bookstore.core.dto.responses.BookDTO;
 import com.example.bookstore.entity.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface BookMapper {
+
+
+    BookMapper INSTANCE = Mappers.getMapper( BookMapper.class );
+
     BookDTO bookToBookDTO(Book book);
 
     Book bookDTOToBook(BookDTO bookDTO);
