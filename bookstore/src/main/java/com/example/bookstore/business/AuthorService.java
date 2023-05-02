51504ffec15727dbe70020df66fd1448c31824f0
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-02 13:53:42 +0300

    general result structures changed

--- bookstore/src/main/java/com/example/bookstore/business/AuthorService.java
+++ bookstore/src/main/java/com/example/bookstore/business/AuthorService.java
@@ -1,15 +1,18 @@
 package com.example.bookstore.business;
 
+import com.example.bookstore.core.dto.requests.CreateAuthorRequest;
+import com.example.bookstore.core.dto.responses.AuthorDTO;
+import com.example.bookstore.core.result.GeneralResult;
 import com.example.bookstore.entity.Author;
 
 import java.util.List;
 
 public interface AuthorService {
-    Author add(Author author);
+    GeneralResult add(CreateAuthorRequest request);
 
-    List<Author> getAll();
+    GeneralResult getAll();
 
-    Author getById(long id);
+    GeneralResult getById(long id);
 
     void delete(long id);
 }
--- bookstore/src/main/java/com/example/bookstore/business/BookService.java
+++ bookstore/src/main/java/com/example/bookstore/business/BookService.java
@@ -1,15 +1,17 @@
 package com.example.bookstore.business;
 
+import com.example.bookstore.core.dto.requests.CreateBookRequest;
+import com.example.bookstore.core.result.GeneralResult;
 import com.example.bookstore.entity.Book;
 
 import java.util.List;
 
 public interface BookService {
-    Book add(Book book);
+    GeneralResult add(CreateBookRequest request);
 
-    List<Book> getAll();
+    GeneralResult getAll();
 
-    Book getById(long id);
+    GeneralResult getById(long id);
 
     void delete(long id);
 }
