40e209ccbdfe303d11c700a2d967da72b3743665
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-02 13:54:06 +0300

    general result structures changed

--- bookstore/src/main/java/com/example/bookstore/business/Impl/AuthorServiceImpl.java
+++ bookstore/src/main/java/com/example/bookstore/business/Impl/AuthorServiceImpl.java
@@ -1,6 +1,14 @@
 package com.example.bookstore.business.Impl;
 
 import com.example.bookstore.business.AuthorService;
+import com.example.bookstore.core.dto.requests.CreateAuthorRequest;
+import com.example.bookstore.core.dto.responses.AuthorDTO;
+import com.example.bookstore.core.exception.AlreadyExistsException;
+import com.example.bookstore.core.exception.EntityNotFoundException;
+import com.example.bookstore.core.mapper.AuthorMapper;
+import com.example.bookstore.core.message.AuthorMessages;
+import com.example.bookstore.core.result.DataResult;
+import com.example.bookstore.core.result.GeneralResult;
 import com.example.bookstore.entity.Author;
 import com.example.bookstore.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,42 +21,46 @@ import java.util.List;
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
 
-
     @Override
-    public Author add(Author author) {
-        checkIfAuthorExistsByName(author.getName());
-        return authorRepository.save(author);
+    public GeneralResult add(CreateAuthorRequest request) {
+        checkIfAuthorExistsByName(request.getName());
+        Author author = AuthorMapper.MAPPER.createAuthorRequestToAuthor(request);
+        authorRepository.save(author);
+        AuthorDTO dto = AuthorMapper.MAPPER.authorToAuthorDTO(author);
+        return new DataResult<>(dto, AuthorMessages.SUCCESSFUL.toString());
+
     }
 
     @Override
-    public List<Author> getAll() {
+    public GeneralResult getAll() {
+        List<Author> authors = authorRepository.findAll();
+        List<AuthorDTO> responses = authors.stream().map(AuthorMapper.MAPPER::authorToAuthorDTO).toList();
 
-        return authorRepository.findAll();
+
+        return new DataResult<>(responses,AuthorMessages.SUCCESSFUL.toString());
     }
 
     @Override
-    public Author getById(long id) {
+    public GeneralResult getById(long id) {
         checkIfAuthorExists(id);
         Author author = authorRepository.findById(id).orElseThrow();
-        return author;
+        return new DataResult<>(author);
     }
-
 
     @Override
     public void delete(long id) {
-     authorRepository.deleteById(id);
+        authorRepository.deleteById(id);
     }
 
-    private void checkIfAuthorExistsByName(String name) {
+    private void checkIfAuthorExistsByName(String name) throws AlreadyExistsException {
         if (authorRepository.existsByNameIgnoreCase(name)) {
-            throw new RuntimeException("bu isimde bir yazar zaten kayıtlı");
+            throw new AlreadyExistsException(AuthorMessages.ALREADY_EXISTS.toString());
         }
     }
 
     private void checkIfAuthorExists(long id) {
         if (!authorRepository.existsById(id)) {
-            throw new RuntimeException("böyle bir yazar bulunamadı");
+            throw new EntityNotFoundException(AuthorMessages.NOT_FOUND.toString());
         }
     }
-
 }
--- bookstore/src/main/java/com/example/bookstore/business/Impl/BookServiceImpl.java
+++ bookstore/src/main/java/com/example/bookstore/business/Impl/BookServiceImpl.java
@@ -1,6 +1,13 @@
 package com.example.bookstore.business.Impl;
 
 import com.example.bookstore.business.BookService;
+import com.example.bookstore.core.dto.requests.CreateBookRequest;
+import com.example.bookstore.core.dto.responses.BookDTO;
+import com.example.bookstore.core.exception.EntityNotFoundException;
+import com.example.bookstore.core.mapper.BookMapper;
+import com.example.bookstore.core.message.BookMessages;
+import com.example.bookstore.core.result.DataResult;
+import com.example.bookstore.core.result.GeneralResult;
 import com.example.bookstore.entity.Book;
 import com.example.bookstore.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,30 +21,38 @@ public class BookServiceImpl implements BookService {
     private final BookRepository repository;
 
     @Override
-    public Book add(Book book) {
-       
-        return repository.save(book);
+    public GeneralResult add(CreateBookRequest request) {
+        Book book = BookMapper.INSTANCE.createBookRequestToBook(request);
+        repository.save(book);
+        BookDTO dto = BookMapper.INSTANCE.bookToBookDTO(book);
+
+
+        return new DataResult<>(dto, BookMessages.SUCCESSFUL.toString());
     }
 
     @Override
-    public List<Book> getAll() {
-        return repository.findAll();
+    public GeneralResult getAll() {
+        List<Book> books = repository.findAll();
+        List<BookDTO> dtoList = books.stream().map(BookMapper.INSTANCE::bookToBookDTO).toList();
+        return new DataResult<>(dtoList);
     }
 
     @Override
-    public Book getById(long id) {
+    public GeneralResult getById(long id) {
         checkIfBookExists(id);
-        Book book=repository.findById(id).orElseThrow();
-        return book;
+        Book book = repository.findById(id).orElseThrow();
+        return new DataResult<>(repository.findById(id), BookMessages.SUCCESSFUL.toString());
     }
+
     @Override
     public void delete(long id) {
         repository.deleteById(id);
 
     }
+
     private void checkIfBookExists(long id) {
-        if(!repository.existsById(id)){
-            throw new RuntimeException("böyle bir kitap bulunamadı");
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException(BookMessages.NOT_FOUND.toString());
         }
     }
 }
