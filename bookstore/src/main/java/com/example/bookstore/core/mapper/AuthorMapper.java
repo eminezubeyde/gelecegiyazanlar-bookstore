70313005709e42139cb6e1d77906eb122ea0961e
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-02 13:54:58 +0300

    methods added

--- bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
+++ bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
@@ -1,12 +1,22 @@
 package com.example.bookstore.core.mapper;
 
+import com.example.bookstore.core.dto.requests.CreateAuthorRequest;
 import com.example.bookstore.core.dto.responses.AuthorDTO;
 import com.example.bookstore.entity.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AuthorMapper {
+
+    static AuthorMapper MAPPER = Mappers.getMapper(AuthorMapper.class);
+
+
     AuthorDTO authorToAuthorDTO(Author author);
 
     Author authorDTOToAuthor(AuthorDTO authorDTO);
+
+    AuthorDTO authorCreateRequestToAuthorDTO(CreateAuthorRequest request);
+
+    Author createAuthorRequestToAuthor(CreateAuthorRequest request);
 }
--- bookstore/src/main/java/com/example/bookstore/core/mapper/BookMapper.java
+++ bookstore/src/main/java/com/example/bookstore/core/mapper/BookMapper.java
@@ -1,5 +1,6 @@
 package com.example.bookstore.core.mapper;
 
+import com.example.bookstore.core.dto.requests.CreateBookRequest;
 import com.example.bookstore.core.dto.responses.BookDTO;
 import com.example.bookstore.entity.Book;
 import org.mapstruct.Mapper;
@@ -8,10 +9,11 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface BookMapper {
 
-
-    BookMapper INSTANCE = Mappers.getMapper( BookMapper.class );
+    static BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
     BookDTO bookToBookDTO(Book book);
 
     Book bookDTOToBook(BookDTO bookDTO);
+
+    Book createBookRequestToBook(CreateBookRequest request);
 }
