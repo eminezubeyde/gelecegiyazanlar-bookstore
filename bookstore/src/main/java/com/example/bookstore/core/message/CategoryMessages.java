1fb899c8c8e8e4c93fdd42ed63e40c048266ad41
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-04 20:46:50 +0300

    update

--- bookstore/src/main/java/com/example/bookstore/core/message/CategoryMessages.java
+++ bookstore/src/main/java/com/example/bookstore/core/message/CategoryMessages.java
@@ -3,7 +3,9 @@ package com.example.bookstore.core.message;
 public enum CategoryMessages {
     NOT_FOUND("Böyle bir kategori bulunamadı"),
     ALREADY_EXISTS("Böyle bir kategori zaten kayıtlı"),
-    SUCCESSFUL("Başarıyla eklendi");
+    SUCCESSFUL("Başarıyla eklendi"),
+    NOT_SUCCESSFUL("Böyle bir kategori bulunamadı");
+
 
     private final String message;
 
--- bookstore/src/main/java/com/example/bookstore/repository/CategoryRepository.java
+++ bookstore/src/main/java/com/example/bookstore/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.example.bookstore.repository;
 import com.example.bookstore.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByNameIgnoreCase(String name);
+    Optional<Category> findCategoryByName(String name);
+    Category getCategoryByName(String name);
 }
