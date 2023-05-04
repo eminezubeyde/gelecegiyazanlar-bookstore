2e6d4214dce7460172d86a728320a4b7cd63deaf
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-04 20:43:32 +0300

    update

--- bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
+++ bookstore/src/main/java/com/example/bookstore/core/mapper/AuthorMapper.java
@@ -6,7 +6,7 @@ import com.example.bookstore.entity.Author;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AuthorMapper {
 
     static AuthorMapper MAPPER = Mappers.getMapper(AuthorMapper.class);
